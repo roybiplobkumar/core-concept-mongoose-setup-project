fda56bc1f463f7f6d62a7fbf08de03335eea6a5e
roy-biplobe <biplobkumargk@gmail.com>
2024-07-30 04:42:58 +0600

    basic project setup done

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  port: process.env.PORT,
+  db_url: process.env.DB_URL,
+};
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
