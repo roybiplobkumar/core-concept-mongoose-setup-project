a5056928c06332fab94038b6860e62f549b2a896
roy-biplobe <biplobkumargk@gmail.com>
2024-08-13 02:08:31 +0600

    zod error handle higherorder validatezodRequrest function

--- src/modules/movies/movie.router.ts
+++ src/modules/movies/movie.router.ts
@@ -1,10 +1,24 @@
+import { AnyZodObject } from 'zod';
 import { ReviewControllers } from '../reviews/review.controller';
 import { MovieController } from './movie.controller';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { zodMovieSchema } from './movie.validation';
 
 const router = express.Router();
 
-router.post('/', MovieController.createMovie);
+const validateZodRequest=(schema:AnyZodObject)=>{
+  return async(req:Request, res:Response, next:NextFunction)=>{
+    try{
+      await schema.parse(req.body)
+     next()
+
+    }catch{
+      next()
+    }
+  }
+}
+
+router.post('/', validateZodRequest(zodMovieSchema.creatZodValidationSchema), MovieController.createMovie);
 router.get('/', MovieController.getAllMovies);
 router.get('/:slug', MovieController.getSingleMovieBySlug);
 router.post('/:slug/review', ReviewControllers.addReview);
