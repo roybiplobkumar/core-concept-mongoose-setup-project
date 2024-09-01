d14df088d5d72c899a510d0cf5e09a1d21a56b5d
roy-biplobe <biplobkumargk@gmail.com>
2024-09-02 01:45:03 +0600

    bcriptjs use done

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,4 +4,5 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   db_url: process.env.DB_URL,
+  salt_rounds:process.env.BCRIPT_SALT_ROUNDS
 };
--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -1,38 +1,41 @@
-import { model, Schema } from "mongoose";
-import { TUser } from "./user.interface";
-import { USER_ROLE, USER_STATUS } from "./user.constans";
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
+import { USER_ROLE, USER_STATUS } from './user.constans';
+import bcript from 'bcryptjs';
+import config from '../../config';
 
-
-const userSchema= new Schema<TUser>(
-  {
-    name:{
-      type:String , 
-      required:[true, 'name is require']
-    },
-    role:{
-      type:String,
-      required:[true, 'roll is require'],
-      enum:Object.keys(USER_ROLE)
-    },
-    email:{
-      type:String,
-      required:[true, 'email is required']
-    },
-    password:{
-      type:String,
-      required:[true, 'password is required']
-    },
-  status:{
-    type:String,
-    required:[true, 'status is required '],
-    enum:Object.keys(USER_STATUS),
-    default:USER_STATUS.ACTIVE
+const userSchema = new Schema<TUser>({
+  name: {
+    type: String,
+    required: [true, 'name is require'],
   },
-  PasswordChangeAt:{
-    type:Date
-  }
-  }
-)
+  role: {
+    type: String,
+    required: [true, 'roll is require'],
+    enum: Object.keys(USER_ROLE),
+  },
+  email: {
+    type: String,
+    required: [true, 'email is required'],
+  },
+  password: {
+    type: String,
+    required: [true, 'password is required'],
+  },
+  status: {
+    type: String,
+    required: [true, 'status is required '],
+    enum: Object.keys(USER_STATUS),
+    default: USER_STATUS.ACTIVE,
+  },
+  PasswordChangeAt: {
+    type: Date,
+  },
+});
+userSchema.pre('save', async function(next) {
+   const user=this;
+   user.password= await bcript.hash(user.password, Number(config.salt_rounds));
+   next();
+})
 
-
-export const User=model<TUser>("User", userSchema)
+export const User = model<TUser>('User', userSchema);
